261f818b7277957d34f55392b2b31022e93da93c
Roy Curtis <roy.adrian.curtis+github@gmail.com>
2017-04-06 12:51:55 +0100

    Handle entering grounded boats (incl. on ice)

--- src/main/java/roycurtis/ankkuri/Ankkuri.java
+++ src/main/java/roycurtis/ankkuri/Ankkuri.java
@@ -1,11 +1,15 @@
 package roycurtis.ankkuri;
 
 import org.bukkit.Material;
+import org.bukkit.entity.Boat;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.vehicle.VehicleEnterEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -25,6 +29,7 @@ public class Ankkuri extends JavaPlugin implements Listener
     {
         LOGGER = getLogger();
     }
+    private static final String PERM_BYPASS_RIDEONICE  = "ankkuri.bypass.rideonice";
 
     @Override
     public void onEnable()
@@ -37,7 +42,6 @@ public class Ankkuri extends JavaPlugin implements Listener
     @SuppressWarnings("deprecation")
     public void onBoatCreate(PlayerInteractEvent event)
     {
-        // Handle only placement of boat items
         if (event.getAction() != Action.RIGHT_CLICK_BLOCK)
             return;
 
@@ -48,6 +52,8 @@ public class Ankkuri extends JavaPlugin implements Listener
         Material blockType = event.getClickedBlock().getType();
         Material relType   = event.getClickedBlock().getRelative( event.getBlockFace() ).getType();
         Material itemType  = held.getType();
+
+        // Only handle if held item is at least a boat
         if (itemType != Material.BOAT
             && itemType != Material.BOAT_ACACIA
             && itemType != Material.BOAT_BIRCH
@@ -62,17 +68,60 @@ public class Ankkuri extends JavaPlugin implements Listener
         else if (relType == Material.WATER || relType == Material.STATIONARY_WATER)
             return;
 
+        // Bypass if player is placing on non-water and has the right permission
+        if ( event.getPlayer().hasPermission(PERM_BYPASS_PLACE) )
+            return;
+
         // Bypass if player is placing on ice and has the right permission
         if (blockType == Material.ICE || blockType == Material.PACKED_ICE)
         if ( event.getPlayer().hasPermission(PERM_BYPASS_PLACEONICE) )
             return;
 
-        // Bypass if player is placing on non-water and has the right permission
-        if ( event.getPlayer().hasPermission(PERM_BYPASS_PLACE) )
+        event.getPlayer()
+            .sendMessage("§c*** You don't have permission to place boats on the ground");
+
+        event.setUseItemInHand(Event.Result.DENY);
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    @SuppressWarnings("deprecation")
+    public void onBoatEnter(VehicleEnterEvent event)
+    {
+        if (event.getVehicle().getType() != EntityType.BOAT)
             return;
 
-        event.getPlayer().sendMessage("§c*** You don't have permission to place boats on the ground");
-        event.setUseItemInHand(Event.Result.DENY);
+        if (event.getEntered().getType() != EntityType.PLAYER)
+            return;
+
+        Player   rider     = (Player) event.getEntered();
+        Boat     boat      = (Boat) event.getVehicle();
+        Material blockType = boat.getLocation()
+            .add(0, -0.4, 0)
+            .getBlock().getType();
+
+        // Compensate for tall hitbox blocks (e.g. fences)
+        if (blockType == Material.AIR)
+            blockType = boat.getLocation()
+                .add(0, -1.4, 0)
+                .getBlock().getType();
+
+        // Only handle if boat is on non-water/air surface. Allows air to compensate for waterfalls.
+        if (blockType == Material.WATER
+            || blockType == Material.STATIONARY_WATER
+            || blockType == Material.AIR)
+            return;
+
+        // Bypass if boat on non-water and player has the right permission
+        if ( rider.hasPermission(PERM_BYPASS_RIDE) )
+            return;
+
+        // Bypass if boat on ice and player has the right permission
+        if (blockType == Material.ICE || blockType == Material.PACKED_ICE)
+        if ( rider.hasPermission(PERM_BYPASS_RIDEONICE) )
+            return;
+
+        rider.sendMessage("§c*** You don't have permission to ride grounded boats");
         event.setCancelled(true);
     }
 }
