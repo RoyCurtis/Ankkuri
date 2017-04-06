7bb9a05eba92d13919ce8e7de2b2b401b6add21a
Roy Curtis <roy.adrian.curtis+github@gmail.com>
2017-04-06 17:56:35 +0100

    Move listeners out into their own class

--- src/main/java/roycurtis/ankkuri/Ankkuri.java
+++ src/main/java/roycurtis/ankkuri/Ankkuri.java
@@ -1,38 +1,24 @@
 package roycurtis.ankkuri;
 
-import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
-import org.bukkit.entity.Boat;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
-import org.bukkit.event.Event;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.vehicle.VehicleEnterEvent;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-/** Core class of the Ankkuri plugin. Handles listener creation */
-public class Ankkuri extends JavaPlugin implements Listener
+/** Core class of the Ankkuri plugin */
+public class Ankkuri extends JavaPlugin
 {
-    private static final String PERM_BYPASS_PLACE      = "ankkuri.bypass.place";
-    private static final String PERM_BYPASS_PLACEONICE = "ankkuri.bypass.placeonice";
-    private static final String PERM_BYPASS_RIDE       = "ankkuri.bypass.ride";
-    private static final String PERM_BYPASS_RIDEONICE  = "ankkuri.bypass.rideonice";
-
-    static Ankkuri PLUGIN;
-    static Config  CONFIG;
+    static Ankkuri  PLUGIN;
+    static Config   CONFIG;
+    static Listener LISTENER;
 
     @Override
     public void onEnable()
     {
-        PLUGIN = this;
-        CONFIG = new Config();
-        getServer().getPluginManager().registerEvents(this, this);
+        PLUGIN   = this;
+        CONFIG   = new Config();
+        LISTENER = new Listener();
+        getServer().getPluginManager().registerEvents(LISTENER, this);
     }
 
     @Override
@@ -51,99 +37,5 @@ public class Ankkuri extends JavaPlugin implements Listener
             getServer().getConsoleSender().sendMessage("[Ankkuri] Config reloaded");
 
         return true;
-    }
-
-    /** Has to use PlayerInteractEvent, as VehicleCreateEvent does not track creator */
-    @EventHandler
-    @SuppressWarnings("deprecation")
-    public void onBoatCreate(PlayerInteractEvent event)
-    {
-        if (event.getAction() != Action.RIGHT_CLICK_BLOCK)
-            return;
-
-        ItemStack held = event.getItem();
-        if (held == null)
-            return;
-
-        Material blockType = event.getClickedBlock().getType();
-        Material relType   = event.getClickedBlock().getRelative( event.getBlockFace() ).getType();
-        Material itemType  = held.getType();
-
-        // Only handle if held item is at least a boat
-        if (itemType != Material.BOAT
-            && itemType != Material.BOAT_ACACIA
-            && itemType != Material.BOAT_BIRCH
-            && itemType != Material.BOAT_DARK_OAK
-            && itemType != Material.BOAT_JUNGLE
-            && itemType != Material.BOAT_SPRUCE)
-            return;
-
-        // Only handle if boat is being placed on non-water surface; compensate for underwater
-        if (blockType == Material.WATER || blockType == Material.STATIONARY_WATER)
-            return;
-        else if (relType == Material.WATER || relType == Material.STATIONARY_WATER)
-            return;
-
-        // Bypass if player is placing on non-water and has the right permission
-        if ( event.getPlayer().hasPermission(PERM_BYPASS_PLACE) )
-            return;
-
-        // Bypass if player is placing on ice and has the right permission
-        if (blockType == Material.ICE || blockType == Material.PACKED_ICE)
-        if ( event.getPlayer().hasPermission(PERM_BYPASS_PLACEONICE) )
-            return;
-
-        event.getPlayer()
-            .sendMessage("§c*** You don't have permission to place boats on the ground");
-
-        event.setUseItemInHand(Event.Result.DENY);
-        event.setCancelled(true);
-    }
-
-    @EventHandler
-    @SuppressWarnings("deprecation")
-    public void onBoatEnter(VehicleEnterEvent event)
-    {
-        if (event.getVehicle().getType() != EntityType.BOAT)
-            return;
-
-        Entity   rider     = event.getEntered();
-        Boat     boat      = (Boat) event.getVehicle();
-        Material blockType = boat.getLocation()
-            .add(0, -0.4, 0)
-            .getBlock().getType();
-
-        // Compensate for tall hitbox blocks (e.g. fences)
-        if (blockType == Material.AIR)
-            blockType = boat.getLocation()
-                .add(0, -1.4, 0)
-                .getBlock().getType();
-
-        // Only handle if boat is on non-water/air surface. Allows air to compensate for waterfalls.
-        if (blockType == Material.WATER
-            || blockType == Material.STATIONARY_WATER
-            || blockType == Material.AIR)
-            return;
-
-        // Prevent capturing of mobs in grounded boats, if enabled
-        if (event.getEntered().getType() != EntityType.PLAYER)
-        {
-            if (CONFIG.blockGroundedBoatCapture)
-                event.setCancelled(true);
-
-            return;
-        }
-
-        // Bypass if boat on non-water and player has the right permission
-        if ( rider.hasPermission(PERM_BYPASS_RIDE) )
-            return;
-
-        // Bypass if boat on ice and player has the right permission
-        if (blockType == Material.ICE || blockType == Material.PACKED_ICE)
-        if ( rider.hasPermission(PERM_BYPASS_RIDEONICE) )
-            return;
-
-        rider.sendMessage("§c*** You don't have permission to ride grounded boats");
-        event.setCancelled(true);
     }
 }
